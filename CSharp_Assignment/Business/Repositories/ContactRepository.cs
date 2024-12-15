88a36f6ff5623e52de4516f5f270eaf0159cef75
Slisk Lindqvist <slisk.lindqvist@querzion.com>
2024-12-15 07:14:32 +0100

    FileService seems to be done now, which means it's able to save the list to a json file, have yet to create the feature to create a contact list though.

--- CSharp_Assignment/Business/Repositories/ContactRepository.cs
+++ CSharp_Assignment/Business/Repositories/ContactRepository.cs
@@ -1,8 +1,34 @@
+using System.Diagnostics;
+using System.Text.Json;
+using Business.Entities;
 using Business.Interfaces;
 
 namespace Business.Repositories;
 
-public class ContactRepository : IContactRepository
+public class ContactRepository(IContactFileService contactFileService) : BaseRepository<ContactEntity>, IContactRepository
 {
-    private readonly 
+    private readonly IContactFileService _contactFileService = contactFileService;
+
+    public override bool SaveToFile(List<ContactEntity> list)
+    {
+        try
+        {
+            var json = JsonSerializer.Serialize(list);
+            _contactFileService.SaveContentToFile(json);
+            
+            return true;
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex.Message);
+            return false;
+        }
+    }
+
+    public override List<ContactEntity>? GetFromFile()
+    {
+        var json = _contactFileService.GetContentFromFile();
+        var list = Deserialize(json);
+        return list;
+    }
 }
