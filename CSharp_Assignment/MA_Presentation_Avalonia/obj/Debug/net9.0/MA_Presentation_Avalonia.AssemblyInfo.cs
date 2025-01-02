5fb32d0c7ae9c701a2b1852acf1510554de93fa3
Slisk Lindqvist <slisk.lindqvist@querzion.com>
2025-01-02 20:13:47 +0100

    Added the last parts on the MainWindow in order to get the views visible. I will now take a break. . . If I can.

--- CSharp_Assignment/MA_Presentation_Avalonia/obj/Debug/net9.0/MA_Presentation_Avalonia.AssemblyInfo.cs
+++ CSharp_Assignment/MA_Presentation_Avalonia/obj/Debug/net9.0/MA_Presentation_Avalonia.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Slisk Lindqvist")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c98caa62cd1f76ce418f9925015106087609966")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+994a17eb7a95a08b8866c7ed740909da563d6201")]
 [assembly: System.Reflection.AssemblyProductAttribute("MA_Presentation_Avalonia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MA_Presentation_Avalonia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
